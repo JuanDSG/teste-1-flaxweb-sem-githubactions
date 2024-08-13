72217526a69c6b5d719a6609c4029fae130ecbc2
JuanDSG <juandiegosg@gmail.com>
2024-08-13 15:37:20 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,38 +1,4 @@
 
             // file: index.tsx
-            import React from 'react';
-import * as RN from 'react-native';
-
-// ---------- import Internals
-// import {SRC} from './src/index.tsx'
-
-console.log(">>>>> AQUI X - rodando o código!")
-
-// ---------- set Main Component
-export default function Router() {
-
-  const stl = {
-    // backgroundColor:'blue',
-    backgroundColor:'green',
-    justifyContent:'center', 
-    alignItems:'center', 
-    width:'100%', 
-    height:'100%',
-  }
-
-  // <SRC />
-
-  return (
-    <RN.View style={stl}>
-      <RN.Text style={{color: 'white'}}>{'1 - Olá MOCK 1!'}</RN.Text>
-    </RN.View>  
-  );
-};
-
-
-RN.AppRegistry.registerComponent("Router", () => Router);
-
-RN.AppRegistry.runApplication("Router", {
-rootTag: document.getElementById("root"),
-});
+            undefined
         
