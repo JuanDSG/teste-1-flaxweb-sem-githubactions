155fe89582f3b4725396198299437b06c380b04b
JuanDSG <juandiegosg@gmail.com>
2024-08-14 01:12:30 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@
 // ---------- import React Packs
 import React from 'react';
 import { Text, View } from 'react-native';
+import { tools } from './tools';
 
 export const SRC = () => {
+  
+  tools()
+
   return (
     <>
       <View style={{ height: 10 }} />
