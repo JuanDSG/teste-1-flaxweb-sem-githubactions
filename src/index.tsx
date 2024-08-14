bee2019722ce62a007560ee439044965d3b447fa
JuanDSG <juandiegosg@gmail.com>
2024-08-13 23:40:13 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 
-
 // ---------- import React Packs
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
 export const SRC = () => {
- return (
-  <>
-    <View style={{height:10}} />
-    <Text style={{color:'white'}}>{'Hi from:'}</Text>
-    <Text style={{color:'white'}}>{'The SRC Module!'}</Text>
-  </>
- )
-}
+  return (
+    <>
+      <View style={{ height: 10 }} />
+      <Text style={{ color: 'white' }}>{'Hi from:'}</Text>
+      <Text style={{ color: 'white' }}>{'The SRC Module 1!'}</Text>
+    </>
+  );
+};
+
